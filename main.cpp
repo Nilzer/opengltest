c95361cd66192ff1305a29eef65bd36eadd91da9
Nils Fallgatter <nils.fallgatter@web.de>
2022-07-23 18:30:34 +0200

    Errormessagehandling

--- main.cpp
+++ main.cpp
@@ -19,6 +19,30 @@
 #include "index_buffer.h"
 #include "shader.h"
 
+// Fehlerfunktion für Konsolenausgabe
+void openGlDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam){
+    std::cout << "[OpenGL Error] " << message << std::endl;
+}
+
+#ifdef _DEBUG
+// alte Errormassagehandlingfunktion
+void _GLGetError(const char* file, int line, const char* call){
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] " << glewGetErrorString(error) << " in "  << file << " " << line << " Call: " << call << std::endl;
+    }
+    
+}
+
+// Makro für alte Errormassagehandlingfunktion
+#define GLCALL(call) call; _GLGetError(__FILE__, __LINE__, #call)
+
+#else
+
+#define GLCALL(call) call
+
+#endif
+
 
 int main(int argc, char *argv[]) {
     SDL_Window* window;
@@ -34,6 +58,11 @@ int main(int argc, char *argv[]) {
     // Doublebuffering an
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
+    // Debug verbose
+    #ifdef _DEBUG
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+    #endif
+
     window = SDL_CreateWindow("C++ OpenGL Tut", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
     SDL_GLContext glContext = SDL_GL_CreateContext(window);
 
@@ -47,6 +76,14 @@ int main(int argc, char *argv[]) {
     }
     
     std::cout << "OpenGL-Version:" << glGetString(GL_VERSION) << std::endl;
+
+#ifdef _DEBUG
+    // Debugoutput aktiviert
+    glEnable(GL_DEBUG_OUTPUT);
+    // sofortige Benachrichtigung
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(openGlDebugCallback, 0);
+#endif
 
     // Daten für Vertexbuffer auf GPU-Speicher 
     Vertex vertices[] = {
@@ -85,7 +122,10 @@ int main(int argc, char *argv[]) {
         vertexBuffer.bind();
         indexBuffer.bind();
         // 3D-Objekt zeichnen
-        glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT ,0);
+
+        // Aufruf alter Errormassagehandlingfunktion
+        // GLCALL( glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, 0) );
+        glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, 0);
         indexBuffer.unbind();
         vertexBuffer.unbind();
 
